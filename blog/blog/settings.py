b872c369ac6bfdcdfe918b79ca6a90482d6a47af
Diego Duarte <projectdcd@gmail.com>
2021-04-30 20:37:56 -0500

    added login and logout, changed urls to match djangos built in login for accounts

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -129,6 +129,8 @@ STATICFILES_FINDERS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('posts.urls', 'posts'), namespace='posts')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
