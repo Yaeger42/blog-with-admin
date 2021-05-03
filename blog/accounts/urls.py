33ec38fb6e7e0f0a54d4d9d3f68f30f25d5713e7
Diego Duarte <projectdcd@gmail.com>
2021-05-03 18:20:36 -0500

    CRUD on users completed

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
-from .views import RegisterNewUserView, UserPanelView
+from .views import RegisterNewUserView, UserPanelView, UserEditView, DeleteUserView
 
 urlpatterns = [
-    path('signup/', RegisterNewUserView.as_view(), name='signup'),
-    path('users/', UserPanelView.as_view(), name = 'allUsers')
+    path(
+        route = 'signup/', 
+        view = RegisterNewUserView.as_view(), 
+        name = 'signup'),
+
+    path(
+        route = 'users/', 
+        view = UserPanelView.as_view(), 
+        name = 'allUsers'),
+
+    path(
+        route = 'edit/<pk>', 
+        view = UserEditView.as_view(), 
+        name = 'edit'),
+
+    path(
+        route = '<pk>/delete/',
+        view = DeleteUserView.as_view(),
+        name = 'delete'
+    )
 ]
