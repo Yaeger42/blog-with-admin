adbed58489466b0609b66d509bc1095324f0b5ce
Diego Duarte <projectdcd@gmail.com>
2021-05-05 14:55:06 -0500

    updated heroku app

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 """
 DATABASES = {
     'default': {
@@ -95,7 +96,7 @@ DATABASES = {
 
     }
 }
-
+"""
 
 
 # Password validation
