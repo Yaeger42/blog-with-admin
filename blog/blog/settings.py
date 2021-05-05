d3b7fa47e0a2a92260479ac30d5e6957901d0c1f
Diego Duarte <projectdcd@gmail.com>
2021-05-05 14:02:11 -0500

    added pgsql db

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4574yu%&)4@b!!c%c1(x*d28qf2$ud-o*f)p#ai%*@a9qtm2p)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://fierce-eyrie-92665.herokuapp.com", 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd94d4vh115o5p1',
+        'USER': os.environ['pguser'],
+        'PASSWORD': os.environ['pgpassword'],
+        'HOST': os.environ['pghost'],
+        'PORT': os.environ['pgport']
+
+    }
+}
+
 
 
 # Password validation
