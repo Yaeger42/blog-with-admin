c1e21d8e692d718b35fa19aacb9b947a0e5eedb6
Diego Duarte <projectdcd@gmail.com>
2021-05-05 14:35:40 -0500

    added static configs

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include(('accounts.urls', 'users'), namespace="users")),
 
-] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
