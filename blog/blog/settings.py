d974c1ae23a3acaa27c332f4b11ae25bdc20e6eb
Diego Duarte <projectdcd@gmail.com>
2021-05-05 14:40:35 -0500

    static added

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -152,3 +152,5 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
