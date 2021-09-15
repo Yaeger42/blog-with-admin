692c6be9d6ee068d094021f4c11e27150b853c1c
Diego <51517869+Yaeger42@users.noreply.github.com>
2021-09-14 21:10:01 -0500

    Added blank to the image field to allow post a new item without an image

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=40, blank=False)
     category = models.CharField(max_length=20, blank=True)
-    image = models.ImageField(upload_to = 'posts/photos')
+    image = models.ImageField(upload_to = 'posts/photos', blank=True)
     body = models.TextField(max_length=3000)
     created = models.DateTimeField(auto_now_add = True)
     lastUpdated = models.DateTimeField(auto_now = True)
