d93a6a76e860e8533aee4cfa3f6898b131d4ca91
SaidS <saidsilva1106@gmail.com>
2021-09-24 00:33:16 -0500

    Translation to Spanish

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,7 +95,7 @@ DATABASES = {
         'PORT': os.environ['pgport']
 
     }
-}
+}"""
 
 
 # Password validation
