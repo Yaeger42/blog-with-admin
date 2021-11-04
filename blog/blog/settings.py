704ee27af134b98b522a3625ae2b288f40fa3b8a
Diego Duarte <projectdcd@gmail.com>
2021-11-03 19:18:31 -0600

    Edited the database for testing purposes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbmam6qalmegsc',
-        'USER': os.environ['pguser'],
-        'PASSWORD': os.environ['pgpassword'],
-        'HOST': os.environ['pghost'],
-        'PORT': os.environ['pgport']
 
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dbmam6qalmegsc',
+#         'USER': os.environ['pguser'],
+#         'PASSWORD': os.environ['pgpassword'],
+#         'HOST': os.environ['pghost'],
+#         'PORT': os.environ['pgport']
+
+#     }
+# }
 
 
 # Password validation
