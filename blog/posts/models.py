bb2f8eb3a4d9c8a245a9cca6d44490f7eb13724a
Diego Duarte <projectdcd@gmail.com>
2021-11-03 19:19:14 -0600

    added facebook video  and facebook live attributes

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -8,5 +8,7 @@ class Post(models.Model):
     category = models.CharField(max_length=20, blank=True)
     image = models.ImageField(upload_to = 'posts/photos', blank=True)
     body = models.TextField(max_length=3000)
+    facebookVideoLink = models.CharField(max_length=200, blank=True)
+    facebookLiveVideoLink = models.CharField(max_length=200, blank=True)
     created = models.DateTimeField(auto_now_add = True)
     lastUpdated = models.DateTimeField(auto_now = True)
